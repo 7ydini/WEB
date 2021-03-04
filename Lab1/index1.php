ac693546faffafa1e44329b3bdeaffa0767365e0
7ydini <61342487+7ydini@users.noreply.github.com>
2021-03-04 16:35:55 +0300

    Create index1.php

---
+++ Lab1/index1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <title>Тег table</title>
+  <style> 
+   table {
+    width: 100%; /* Ширина таблицы */
+    background: white; /* Цвет фона таблицы */
+    color: white; /* Цвет текста */
+    border-spacing: 1px; /* Расстояние между ячейками */
+   }
+   td, th {
+    background: graytext; /* Цвет фона ячеек */
+    padding: 5px; /* Поля вокруг текста */
+   }
+  </style>
+ </head> 
+ <body>
+  <table>
+      <link href="https://fonts.google.com/share?selection.family=Open%20Sans:wght@300"rel="stylesheet">
+      <tr><th>Colomn 1</th><th width="100">Colomn 2</th><th width="100">Colomn 3</th></tr>
+  </table>
+ </body>
+</html>
